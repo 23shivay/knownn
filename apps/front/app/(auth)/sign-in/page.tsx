22fba90baaed67d1a12b995a96ad23a355532cec
23shivay <23shivay02@gmail.com>
2025-02-14 17:35:22 +0530

    register message

--- apps/front/app/(auth)/sign-in/page.tsx
+++ apps/front/app/(auth)/sign-in/page.tsx
@@ -129,7 +129,7 @@ export default function SignInForm() {
         </Form>
         <div className="text-center mt-4">
           <p>
-            Not a member?{" "}
+            Register here if New?{" "}
             <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
               Sign up
             </Link>
