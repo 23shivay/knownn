403043326e87da33f3ac5fe1b47d4b3e76451e94
23shivay <23shivay02@gmail.com>
2025-02-15 18:44:09 +0530

    port no

--- apps/server/src/config/redis.config.ts
+++ apps/server/src/config/redis.config.ts
@@ -2,7 +2,7 @@ import {Redis} from "ioredis"
 
 const redis=new Redis({
     host:`${process.env.REDIS_HOST}`,
-    port:22451,
+    port:14731,
     username:`${process.env.REDIS_USERNAME}`,
     password:`${process.env.REDIS_PASSWORD}`
 
